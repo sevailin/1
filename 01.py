04df8b82cc5ef720e266209c9ffd31280776798d
sevailin <49301534+sevailin@users.noreply.github.com>
2022-10-15 17:22:54 +0600

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,19 @@
+from collections import Counter
+
+
+def buyers_count(n_customers, n_first_id=0):
+    group_numbers = []
+
+    for i in range(n_customers):
+        group_number = 0
+        current_id = n_first_id + i
+        for j in range(len(str(current_id))):
+            group_number += current_id % 10
+            current_id = current_id // 10
+        group_numbers.append(group_number)
+
+    groups_count = Counter(group_numbers)
+    for i in dict(groups_count):
+        print("{} : {}".format(i, dict(groups_count)[i]))
+
+
